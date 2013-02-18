dd24e344548d7147d5e5458913c063bd70cf36cc
resttime <resttime@live.com>
2013-02-18 16:57:22 -0600

    Updated Primitives addon

--- liballegro.lisp
+++ liballegro.lisp
@@ -1322,3 +1322,111 @@
     :uint32)
 (defcfun ("al_init_primitives_addon" init-primitives-addon) :boolean)
 (defcfun ("al_shutdown_primitives_addon" shutdown-primitives-addon) :boolean)
+
+;; High level drawing routines
+(defcfun ("al_draw_line" draw-line) :void 
+  (x1 :float) (y1 :float) (x2 :float) (y1 :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_triangle" draw-triangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (x3 :float) (y3 :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_triangle" draw-filled-triangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (x3 :float) (y3 :float)
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_draw_rectangle" draw-rectangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_rectangle" draw-filled-rectangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_draw_rounded_rectangle" draw-rounded-rectangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (rx :float) (ry :float) 
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_rounded_rectangle" draw-filled-rounded-rectangle) :void 
+  (x1 :float) (y1 :float) 
+  (x2 :float) (y2 :float) 
+  (rx :float) (ry :float) 
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_calculate_arc" calculate-arc) :void
+  (dest :pointer) (stride :int) 
+  (cx :float) (cy :float)
+  (rx :float) (ry :float)
+  (start-theta :float) (delta-theta :float)
+  (thickness :float) (num-segments :int))
+(defcfun ("al_draw_pieslice" draw-pieslice) :void 
+  (cx :float) (cy :float) (r :float)
+  (start-theta :float) (delta-theta :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_pieslice" draw-filled-pieslice) :void 
+  (cx :float) (cy :float) (r :float)
+  (start-theta :float) (delta-theta :float)
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_draw_ellipse" draw-ellipse) :void
+  (cx :float) (cy :float) (rx :float) (ry :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_ellipse" draw-filled-ellipse) :void
+  (cx :float) (cy :float) (rx :float) (ry :float)
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_draw_circle" draw-circle) :void
+  (cx :float) (cy :float) (r :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_filled_circle" draw-filled-circle) :void
+  (cx :float) (cy :float) (r :float)
+  (r :float) (g :float) (b :float) (a :float))
+(defcfun ("al_draw_arc" draw-arc) :void 
+  (cx :float) (cy :float) (r :float)
+  (start-theta :float) (delta-theta :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_draw_elliptical_arc" draw-elliptical-arc) :void 
+  (cx :float) (cy :float) (rx :float) (ry :float)
+  (start-theta :float) (delta-theta :float)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_calculate_spline" calculate-spline) :void
+  (dest :pointer) (stride :int) (points :pointer)
+  (thickness :float) (num-segments :int))
+(defcfun ("al_draw_spline" draw-spline) :void
+  (points :pointer)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float))
+(defcfun ("al_calculate_ribbon" calculate-ribbon) :void
+  (dest :pointer) (dest-stride :int) 
+  (points :pointer) (points-stride :int)
+  (thickness :float) (num-segments :int))
+(defcfun ("al_draw_ribbon" draw-ribbon) :void
+  (points :pointer) (points-stride :int)
+  (r :float) (g :float) (b :float) (a :float)
+  (thickness :float) (num-segments :int))
+
+;; Low level drawing routines
+(defcfun ("al_draw_prim" draw-prim) :int 
+  (vtxs :pointer) (decl :pointer) (texture :pointer)
+  (start :int) (end :int) (type :int))
+(defcfun ("al_draw_indexed_prim" draw-indexed-prim) :int 
+  (vtxs :pointer) (decl :pointer) (texture :pointer)
+  (indices :pointer) (num-vtx :int) (type :int))
+(defcfun ("al_create_vertex_decl" create-vertex-decl) :pointer
+  (elements :pointer) (stride :int))
+(defcfun ("al_destroy_vertex_decl" destroy-vertex-decl) :void)
+(defcfun ("al_draw_soft_triangle" draw-soft-triangle) :void
+  (v1 :pointer) (v2 :pointer) (v3 :pointer) (state :pointer)
+  (init :pointer) (first :pointer) (step :pointer) (draw :pointer))
+(defcfun ("al_draw_soft_line" draw-soft-line) :void
+  (v1 :pointer) (v2 :pointer) (state :pointer)
+  (first :pointer) (step :pointer) (draw :pointer))
