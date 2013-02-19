c42c8a093759a8321cccbdc0064c6eeaad64133e
resttime <resttime@live.com>
2013-02-19 16:46:48 -0600

    Added OpenGL section

--- liballegro.lisp
+++ liballegro.lisp
@@ -904,6 +904,26 @@
 ;;; Miscellaneous routines
 (defconstant +allegro-pi+ 3.14159265358979323846)
 
+;;; OpenGL
+(defcfun ("al_get_opengl_extension_list" get-opengl-extension-list) :pointer)
+(defcfun ("al_get_opengl_proc_address" get-opengl-proc-address) :pointer
+  (name :pointer))
+(defcfun ("al_get_opengl_texture" get-opengl-texture) :uint (bitmap :pointer))
+(defcfun ("al_get_opengl_texture_size" get-opengl-texture-size) :void
+  (bitmap :pointer) (width :pointer) (height :pointer))
+(defcfun ("al_get_opengl_texture_position" get-opengl-texture-position) :void
+  (bitmap :pointer) (u :pointer) (v :pointer))
+(defcfun ("al_get_opengl_texture_fbo" get-opengl-texture-fbo) :uint
+  (bitmap :pointer))
+(defcfun ("al_remove_opengl_texture_fbo" remove-opengl-texture-fbo) :uint
+  (bitmap :pointer))
+(defcfun ("al_have_opengl_extension" have-opengl-extension) :boolean
+  (extension :pointer))
+(defcfun ("al_get_opengl_version" get-opengl_version) :uint32)
+(defcfun ("al_get_opengl_variant" get-opengl-variant) :int)
+(defcfun ("al_set_current_opengl_context" set-current-opengl-context) :void
+  (display :pointer))
+
 ;;; Audio addon
 (defcenum allegro-audio-depth
   (:allegro-audio-depth-int8      #x00)
