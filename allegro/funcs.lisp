1340f7e4742cb042dcf94698d312e1eeba211a67
rest <resttim3@gmail.com>
2013-10-20 02:53:40 -0500

    Added color addon

--- allegro/funcs.lisp
+++ allegro/funcs.lisp
@@ -203,7 +203,7 @@
 (defcfun ("al_get_bitmap_format" get-bitmap-format) pixel-format (bitmap :pointer))
 (defcfun ("al_get_bitmap_height" get-bitmap-height) :int (bitmap :pointer))
 (defcfun ("al_get_bitmap_width" get-bitmap-width) :int (bitmap :pointer))
-
+(defcfun ("al_get_pixel") (:struct color) (bitmap :pointer) (x c-int) (y c-int))
 (defcfun ("al_is_bitmap_locked" is-bitmap-locked) :boolean (bitmap :pointer))
 (defcfun ("al_is_compatible_bitmap" is-compatible-bitmap) :boolean (bitmap :pointer))
 (defcfun ("al_is_sub_bitmap" is-sub-bitmap) :boolean (bitmap :pointer))
@@ -778,6 +778,51 @@
 ;;; Audio codecs addon
 (defcfun ("al_init_acodec_addon" init-acodec-addon) :boolean)
 (defcfun ("al_get_allegro_acodec_version" get-allegro-acodec-version) :uint32)
+(defcfun ("al_color_cmyk_to_rgb" color-cmyk-to-rgb) :void
+  (cyan c-float) (magenta c-float) (yellow c-float) (key c-float)
+  (red :pointer) (green :pointer) (blue :pointer))
+(defcfun ("al_color_hsl" color-hsl) (:struct color)
+  (h c-float) (s c-float) (l c-float))
+(defcfun ("al_color_hsl_to_rgb" color-hsl-to-rgb) :void
+  (hue c-float) (saturation c-float) (lightness c-float)
+  (red :pointer) (green :pointer) (blue :pointer))
+(defcfun ("al_color_hsl" color-hsv) (:struct color)
+  (h c-float) (s c-float) (r c-float))
+(defcfun ("al_color_hsv_to_rgb" color-hsv-to-rgb) :void
+  (hue c-float) (saturation c-float) (value c-float)
+  (red :pointer) (green :pointer) (blue :pointer))
+(defcfun ("al_color_html" color-html) (:struct color) (string :string))
+(defcfun ("al_color_html_to_rgb" color-html-to-rgb) :void
+  (string :string) (red :pointer) (green :pointer) (blue :pointer))
+(defcfun ("al_color_rgb_to_html" color-rgb-to-html) :void
+  (red c-float) (green c-float) (blue c-float) (string :string))
+(defcfun ("al_color_name" color-name) (:struct color) (name :string))
+(defcfun ("al_color_name_to_rgb" color-name-to-rgb) :boolean
+  (name :string) (r :pointer) (g :pointer) (b :pointer))
+(defcfun ("al_color_rgb_to_cmyk" color-rgb-to-cmyk) :void
+  (red c-float) (green c-float) (blue c-float)
+  (cyan :pointer) (magenta :pointer) (yellow :pointer) (key :pointer))
+(defcfun ("al_color_rgb_to_hsl" color-rgb-to-hsl) :void
+  (red c-float) (green c-float) (blue c-float)
+  (hue :pointer) (saturation :pointer) (lightness :pointer))
+(defcfun ("al_color_rgb_to_hsv" color-rgb-to-hsv) :void
+  (red c-float) (green c-float) (blue c-float)
+  (hue :pointer) (saturation :pointer) (value :pointer))
+(defcfun ("al_color_rgb_to_name" color-rgb-to-name) :string
+  (r c-float) (g c-float) (b c-float))
+(defcfun ("al_color_rgb_to_yuv" color-rgb-to-yuv) :void
+  (red c-float) (green c-float) (blue c-float)
+  (y :pointer) (u :pointer) (v :pointer))
+(defcfun ("al_color_yuv" color-yuv) (:struct color)
+  (y c-float) (u c-float) (v c-float))
+(defcfun ("al_color_yuv_to_rgb" color-yuv-to-rgb) (:struct color)
+  (y c-float) (u c-float) (v c-float)
+  (red :pointer) (green :pointer) (blue :pointer))
+(defcfun ("al_get_allegro_color_version" get-allegro-color-version) :uint32)
+
+;;; Color addon
+(defcfun ("al_color_cmyk" color-cymk) (:struct color)
+  (c c-float) (m c-float) (y c-float) (k c-float))
 
 ;;; Font addons
 ;; General font routinues
