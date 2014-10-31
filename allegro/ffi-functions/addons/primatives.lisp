f650dfa4d2ca31a9af1c9daaab14ee891ca3eafb
Rest <resttime@live.com>
2014-10-31 01:41:18 -0500

    Completed conversion to use libffi!

--- allegro/ffi-functions/addons/primatives.lisp
+++ allegro/ffi-functions/addons/primatives.lisp
@@ -10,39 +10,39 @@
 ;; High level drawing routines
 (defcfun ("al_draw_line" draw-line) :void 
   (x1 c-float) (y1 c-float) (x2 c-float) (y2 c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_triangle" draw-triangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
   (x3 c-float) (y3 c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_triangle" draw-filled-triangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
   (x3 c-float) (y3 c-float)
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_draw_rectangle" draw-rectangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_rectangle" draw-filled-rectangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_draw_rounded_rectangle" draw-rounded-rectangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
   (rx c-float) (ry c-float) 
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_rounded_rectangle" draw-filled-rounded-rectangle) :void 
   (x1 c-float) (y1 c-float) 
   (x2 c-float) (y2 c-float) 
   (rx c-float) (ry c-float) 
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_calculate_arc" calculate-arc) :void
   (dest :pointer) (stride :int) 
   (cx c-float) (cy c-float)
@@ -52,42 +52,42 @@
 (defcfun ("al_draw_pieslice" draw-pieslice) :void 
   (cx c-float) (cy c-float) (radius c-float)
   (start-theta c-float) (delta-theta c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_pieslice" draw-filled-pieslice) :void 
   (cx c-float) (cy c-float) (radius c-float)
   (start-theta c-float) (delta-theta c-float)
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_draw_ellipse" draw-ellipse) :void
   (cx c-float) (cy c-float) (rx c-float) (ry c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_ellipse" draw-filled-ellipse) :void
   (cx c-float) (cy c-float) (rx c-float) (ry c-float)
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_draw_circle" draw-circle) :void
   (cx c-float) (cy c-float) (radius c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_filled_circle" draw-filled-circle) :void
   (cx c-float) (cy c-float) (radius c-float)
-  (color :pointer))
+  (colot (:struct :color))
 (defcfun ("al_draw_arc" draw-arc) :void 
   (cx c-float) (cy c-float) (radius c-float)
   (start-theta c-float) (delta-theta c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_draw_elliptical_arc" draw-elliptical-arc) :void 
   (cx c-float) (cy c-float) (rx c-float) (ry c-float)
   (start-theta c-float) (delta-theta c-float)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_calculate_spline" calculate-spline) :void
   (dest :pointer) (stride :int) (points :pointer)
   (thickness c-float) (num-segments :int))
 (defcfun ("al_draw_spline" draw-spline) :void
   (points :pointer)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float))
 (defcfun ("al_calculate_ribbon" calculate-ribbon) :void
   (dest :pointer) (dest-stride :int) 
@@ -95,7 +95,7 @@
   (thickness c-float) (num-segments :int))
 (defcfun ("al_draw_ribbon" draw-ribbon) :void
   (points :pointer) (points-stride :int)
-  (color :pointer)
+  (colot (:struct :color)
   (thickness c-float) (num-segments :int))
 
 ;; Low level drawing routines
