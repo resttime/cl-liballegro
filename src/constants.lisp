490d38ca61c44d5b256236f581bb716c98e29435
resttime <resttime@users.noreply.github.com>
2016-04-25 02:53:45 -0500

    Update constants and Events section for 5.2

--- src/constants.lisp
+++ src/constants.lisp
@@ -85,16 +85,20 @@
   (:joystick-button-down 2)
   (:joystick-button-up 3)
   (:joystick-configuration 4)
+  
   (:key-down 10)
   (:key-char 11)
   (:key-up 12)
+  
   (:mouse-axis 20)
   (:mouse-button-down 21)
   (:mouse-button-up 22)
   (:mouse-enter-display 23)
   (:mouse-leave-display 24)
   (:mouse-warped 25)
+  
   (:timer 30)
+  
   (:display-expose 40)
   (:display-resize 41)
   (:display-close 42)
@@ -102,7 +106,17 @@
   (:display-found 44)
   (:display-switch-in 45)
   (:display-switch-out 46)
-  (:display-orientation 47))
+  (:display-orientation 47)
+  (:display-halt-drawing 48)
+  (:display-resume-drawing 49)
+
+  (:touch-begin 50)
+  (:touch-end 51)
+  (:touch-move 52)
+  (:touch-cancel 53)
+   
+  (:display-connected 60)
+  (:display-disconnected 61))
   
 ;;; File I/O
 (defcenum seek
