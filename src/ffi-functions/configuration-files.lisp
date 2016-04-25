05d216a8311e9e8c1cf769442cb185c1bf3efa82
resttime <resttime@users.noreply.github.com>
2016-04-25 01:48:32 -0500

    Begin adding Allegro 5.2 support with config files

--- src/ffi-functions/configuration-files.lisp
+++ src/ffi-functions/configuration-files.lisp
@@ -8,21 +8,25 @@
   (filename :string) (config :pointer))
 (defcfun ("al_save_config_file_f" save-config-file-f) :boolean
   (file :pointer) (config :pointer))
-(defcfun ("al_add_config_section" add_config_section) :void
+(defcfun ("al_add_config_section" add-config-section) :void
   (config :pointer) (name :string))
+(defcfun ("al_remove_config_section" remove-config-section) :void
+  (config :pointer) (section :string))
 (defcfun ("al_add_config_comment" add_config_comment) :void
   (config :pointer) (section :string) (comment :string))
 (defcfun ("al_get_config_value" get-config-value) :string
   (config :pointer) (section :string) (key :string))
 (defcfun ("al_set_config_value" set-config-value) :void
   (config :pointer) (section :string) (key :string) (value :string))
+(defcfun ("al_remove_config_key" remove-config-key) :bool
+  (config :pointer) (section :string) (key :string))
 (defcfun ("al_get_first_config_section" get-first-config-section) :string
   (config :pointer) (iterator :pointer))
 (defcfun ("al_get_next_config_section" get-next-config-section) :string
   (iterator :pointer))
 (defcfun ("al_get_first_config_entry" get-first-config-entry) :string
   (config :pointer) (section :string) (iterator :pointer))
-(defcfun ("al_get_next_config_entry" get_next_config_entry) :pointer
+(defcfun ("al_get_next_config_entry" get-next-config-entry) :pointer
   (iterator :pointer))
 (defcfun ("al_merge_config" merge-config) :pointer
   (cfg1 :pointer) (cfg2 :pointer))
