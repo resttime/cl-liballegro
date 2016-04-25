db0bbfe09026773d64846e5ec5897c52296b7408
resttime <resttime@users.noreply.github.com>
2016-04-25 02:37:28 -0500

    Update Display section for 5.2.

--- src/constants.lisp
+++ src/constants.lisp
@@ -2,18 +2,23 @@
 ;;; Display
 ;; Flags
 (defbitfield display-flags
-    :windowed
-    :fullscreen
-    :opengl
-    :direct3d-internal
-    :resizable
-    :frameless
-    (:noframe 32)
-    :generate-expose-events
-    :opengl-3-0
-    :opengl-forward-compatible
-    :fullscreen-window
-    :minimized)
+  :windowed
+  :fullscreen
+  :opengl
+  :direct3d-internal
+  :resizable
+  :frameless
+  (:noframe 32)
+  :generate-expose-events
+  :opengl-3-0
+  :opengl-forward-compatible
+  :fullscreen-window
+  :minimized
+  :programmable-pipeline
+  :gtk-toplevel-internal
+  :maximized
+  :opengl-es-profile)
+
 ;; Display Options
 (defcenum display-options
   :red-size
@@ -47,6 +52,10 @@
   :support-npot-bitmap
   :can-draw-into-bitmap
   :support-separate-alpha
+  :auto-convert-bitmaps
+  :supported-orientations
+  :opengl-major-version
+  :opengl-minor-version
   :display-options-count)
 
 ;; Importance
@@ -57,12 +66,18 @@
 
 ;; Display Orientation
 (defcenum display-orientation
-  :0-degrees
-  :90-degrees
-  :180-degrees
-  :270-degrees
-  :face-up
-  :face-down)
+  (:unknown 0)
+  (:0-degrees 1)
+  (:90-degrees 2)
+  (:180-degrees 4)
+  (:270-degrees 8)
+  (:portrait 5)
+  (:landscape 10)
+  (:all 15)
+  (:face-up 16)
+  (:face-down 32))
+
+(defconstant +new-window-title-max-size+ 255)
 
 ;;; Events
 (defcenum event-types
