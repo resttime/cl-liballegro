1ac19ddae2fe003bf7b9c6b6f34d15ac85523196
resttime <resttime@users.noreply.github.com>
2020-03-25 12:32:58 -0400

    Use a macro for defining allegro libraries
    Should help with any versioning changes in the future

--- src/library.lisp
+++ src/library.lisp
@@ -1,81 +1,59 @@
 (in-package #:cl-liballegro)
 
-(define-foreign-library liballegro
-  (:windows "allegro-5.2.dll")
-  (:unix (:or "liballegro.so.5.2.2" "liballegro.so.5.2"))
-  (t (:default "liballegro")))
+(defmacro define-allegro-library (lib)
+  "Use macro for easier versioning.  Debug using MACROEXPAND-1"
+  `(define-foreign-library ,(intern (string-upcase lib))
+     (:windows ,(concatenate 'string (subseq lib 3) "-5.2.dll"))
+     (:unix (:or ,(concatenate 'string lib ".so.5.2.6")
+                 ,(concatenate 'string lib ".so.5.2")
+                 ,(concatenate 'string lib ".so")))
+     (t (:default ,(concatenate 'string lib)))))
+;; ;; Example
+;; (macroexpand-1 '(define-allegro-library "liballegro"))
+;;
+;; (DEFINE-FOREIGN-LIBRARY LIBALLEGRO
+;;   (:WINDOWS "allegro-5.2.dll")
+;;   (:UNIX (:OR "liballegro.so.5.2" "liballegro.so"))
+;;   (T (:DEFAULT "liballegro")))
+
+
+(define-allegro-library "liballegro")
 (use-foreign-library liballegro)
 
-(define-foreign-library liballegro-main
-  (:windows "allegro_main-5.2.dll")
-  (:unix (:or "liballegro_main.so.5.2.2" "liballegro_main.so.5.2"))
-  (t (:default "liballegro_main")))
-(use-foreign-library liballegro-main)
+(define-allegro-library "liballegro_main")
+(use-foreign-library liballegro_main)
 
-(define-foreign-library liballegro-acodec
-  (:windows "allegro_acodec-5.2.dll")
-  (:unix (:or "liballegro_acodec.so.5.2.2" "liballegro_acodec.so.5.2"))
-  (t (:default "liballegro_acodec")))
-(use-foreign-library liballegro-acodec)
+(define-allegro-library "liballegro_acodec")
+(use-foreign-library liballegro_acodec)
 
-(define-foreign-library liballegro-audio
-  (:windows "allegro_audio-5.2.dll")
-  (:unix (:or "liballegro_audio.so.5.2.2" "liballegro_audio.so.5.2"))
-  (t (:default "liballegro_audio")))
-(use-foreign-library liballegro-audio)
+(define-allegro-library "liballegro_audio")
+(use-foreign-library liballegro_audio)
 
-(define-foreign-library liballegro-color
-  (:windows "allegro_color-5.2.dll")
-  (:unix (:or "liballegro_color.so.5.2.2" "liballegro_color.so.5.2"))
-  (t (:default "liballegro_color")))
-(use-foreign-library liballegro-color)
+(define-allegro-library "liballegro_color")
+(use-foreign-library liballegro_color)
 
-(define-foreign-library liballegro-dialog
-  (:windows "allegro_dialog-5.2.dll")
-  (:unix (:or "liballegro_dialog.so.5.2.2" "liballegro_dialog.so.5.2"))
-  (t (:default "liballegro_dialog")))
-(use-foreign-library liballegro-dialog)
+(define-allegro-library "liballegro_dialog")
+(use-foreign-library liballegro_dialog)
 
-(define-foreign-library liballegro-font
-  (:windows "allegro_font-5.2.dll")
-  (:unix (:or "liballegro_font.so.5.2.2" "liballegro_font.so.5.2"))
-  (t (:default "liballegro_font")))
-(use-foreign-library liballegro-font)
+(define-allegro-library "liballegro_font")
+(use-foreign-library liballegro_font)
 
-(define-foreign-library liballegro-image
-  (:windows "allegro_image-5.2.dll")
-  (:unix (:or "liballegro_image.so.5.2.2" "liballegro_image.so.5.2"))
-  (t (:default "liballegro_image")))
-(use-foreign-library liballegro-image)
+(define-allegro-library "liballegro_image")
+(use-foreign-library liballegro_image)
 
-(define-foreign-library liballegro-memfile
-  (:windows "allegro_memfile-5.2.dll")
-  (:unix (:or "liballegro_memfile.so.5.2.2" "liballegro_memfile.so.5.2"))
-  (t (:default "liballegro_memfile")))
-(use-foreign-library liballegro-memfile)
+(define-allegro-library "liballegro_memfile")
+(use-foreign-library liballegro_memfile)
 
-(define-foreign-library liballegro-physfs
-  (:windows "allegro_physfs-5.2.dll")
-  (:unix (:or "liballegro_physfs.so.5.2.2" "liballegro_physfs.so.5.2"))
-  (t (:default "liballegro_physfs")))
-(handler-case (use-foreign-library liballegro-physfs)
+(define-allegro-library "liballegro_physfs")
+(handler-case (use-foreign-library liballegro_physfs)
   (error (condition) (print "physfs library not found") condition))
 
-(define-foreign-library liballegro-primitives
-  (:windows "allegro_primitives-5.2.dll")
-  (:unix (:or "liballegro_primitives.so.5.2.2" "liballegro_primitives.so.5.2"))
-  (t (:default "liballegro_primitives")))
-(use-foreign-library liballegro-primitives)
+(define-allegro-library "liballegro_primitives")
+(use-foreign-library liballegro_primitives)
 
-(define-foreign-library liballegro-ttf
-  (:windows "allegro_ttf-5.2.dll")
-  (:unix (:or "liballegro_ttf.so.5.2.2" "liballegro_ttf.so.5.2"))
-  (t (:default "liballegro_ttf")))
-(use-foreign-library liballegro-ttf)
+(define-allegro-library "liballegro_ttf")
+(use-foreign-library liballegro_ttf)
 
-(define-foreign-library liballegro-video
-  (:windows "allegro_video-5.2.dll")
-  (:unix (:or "liballegro_video.so.5.2.2" "liballegro_video.so"))
-  (t (:default "liballegro_video")))
-(handler-case (use-foreign-library liballegro-video)
+(define-allegro-library "liballegro_video")
+(handler-case (use-foreign-library liballegro_video)
   (error (condition) (print "Video Addon Available only in versions >=5.1.0") condition))
