4dd29d1710c48487d4a6fbf0f20255f1059582d4
resttime <resttime@users.noreply.github.com>
2020-05-04 02:12:25 -0400

    Add more type hints to the shader function parameters

--- src/ffi-functions/shader.lisp
+++ src/ffi-functions/shader.lisp
@@ -4,15 +4,15 @@
 (defcfun ("al_create_shader" create-shader) :pointer
   (platform shader-platform))
 (defcfun ("al_attach_shader_source" attach-shader-source) :bool
-  (shader :pointer) (type shader-type) (source :string))
+  (shader (:pointer (:struct shader))) (type shader-type) (source :string))
 (defcfun ("al_attach_shader_source_file" attach-shader-source-file) :bool
-  (shader :pointer) (type shader-type) (filename :string))
+  (shader (:pointer (:struct shader))) (type shader-type) (filename :string))
 (defcfun ("al_build_shader" build-shader) :bool
-  (shader :pointer))
+  (shader (:pointer (:struct shader))))
 (defcfun ("al_get_shader_log" get-shader-log) :string
-  (shader :pointer))
+  (shader (:pointer (:struct shader))))
 (defcfun ("al_get_shader_platform" get-shader-platform) shader-platform
-  (shader :pointer))
+  (shader (:pointer (:struct shader))))
 (defcfun ("al_use_shader" use-shader) :bool (shader :pointer))
 (defcfun ("al_destroy_shader" destroy-shader) :void (shader :pointer))
 (defcfun ("al_set_shader_sampler" set-shader-sampler) :bool
@@ -26,8 +26,8 @@
 (defcfun ("al_set_shader_bool" set-shader-bool) :bool
   (name :string) (b :bool))
 (defcfun ("al_set_shader_int_vector" set-shader-int-vector) :bool
-  (name :string) (num-components :int) (i :pointer) (num-elems :int))
+  (name :string) (num-components :int) (i (:pointer :int)) (num-elems :int))
 (defcfun ("al_set_shader_float_vector" set-shader-float-vector) :bool
-  (name :string) (num-components :int) (f :pointer) (num-elems :int))
+  (name :string) (num-components :int) (f (:pointer :float)) (num-elems :int))
 (defcfun ("al_get_default_shader_source" get-default-shader-source) :string
   (platform shader-platform) (type shader-type))
