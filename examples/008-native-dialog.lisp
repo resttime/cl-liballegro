62e006d9c63f5cfb34c5f4b5bb66f327488b8584
resttime <resttime@users.noreply.github.com>
2023-08-23 17:10:04 -0500

    Add example for using show-native-message-box

---
+++ examples/008-native-dialog.lisp
@@ -0,0 +1,13 @@
+(ql:quickload :cl-liballegro)
+
+(defun display-msg-box ()
+  "View the function definition for more details:
+https://liballeg.org/a5docs/trunk/native_dialog.html#al_show_native_message_box
+
+Flags are defined in src/constants/addons/native-dialogs.lisp"
+  (al:init)
+  (al:init-native-dialog-addon)
+  (let ((display (al:create-display 800 600)))
+    (al:show-native-message-box display "Title" "Heading" "Text" (cffi:null-pointer) 0)
+    (al:destroy-display display))
+  (al:shutdown-native-dialog-addon))
